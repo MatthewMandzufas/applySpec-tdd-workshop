32a408b896b51cd1a697bd6182a7986b40e838be
Matthew Mandzufas <matthew.mandzufas@journeyone.com.au>
2024-04-19 11:22:50 +0800

    RED: applySpec, works with unary functions

--- src/applySpec.test.js
+++ src/applySpec.test.js
@@ -1,7 +1,14 @@
+const R = require('ramda');
 import applySpec from './applySpec';
 
 describe('applySpec', () => {
     it('works with an empty spec', () => {
         expect(applySpec({})()).toEqual({});
+    });
+    it('works with unary functions', () => {
+        expect(applySpec({ v: R.inc, u: R.dec })(1)).toEqual({
+            v: 2,
+            u: 0,
+        });
     });
 });
